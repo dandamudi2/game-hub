c0d50bf38059e078b82603d83feca2f9761cecfe
dandamudi2 <srinivasd2mail@gmail.com>
2023-10-25 22:40:19 +0800

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,7 +17,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={2}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
--- src/components/Genere.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import useGeneres from '../hooks/useGenres';
-
-const Genere = () => {
-  const { genres, error, isLoading } = useGeneres();
-  return <div></div>;
-};
-
-export default Genere;
